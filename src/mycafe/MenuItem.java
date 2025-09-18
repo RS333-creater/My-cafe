49b88b4a63305d6ea77200910c2baf9a1f40b8ab
samurai <150229185+RS333-creater@users.noreply.github.com>
2025-09-18 11:53:24 +0900

    new

---
+++ src/mycafe/MenuItem.java
@@ -0,0 +1,21 @@
+package mycafe;
+
+public abstract class MenuItem {
+	private String name;
+	private int price;
+	
+	public MenuItem(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getPrice() {
+		return price;
+	}
+	
+	public abstract String getDetails(); 
+}
